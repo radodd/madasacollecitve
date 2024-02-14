0dd95363862d197fd2aa041b5072ca2905359c9a
Daniel Castillo <danny4004@gmail.com>
2024-02-13 22:57:37 -0800

    first commit

---
+++ index.ts
@@ -0,0 +1,3 @@
+export const whoWeAre=[
+    "We are a freelance team of designers and developers who work with clients to create digital experiences that are affordable, and exactly what the client wants."
+]
