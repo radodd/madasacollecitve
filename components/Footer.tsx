17f40d9dd7ffae4ab783584603227eb25b8e5aa6
radodd <ethanf.flores@gmail.com>
2024-02-20 16:36:53 -0800

    test vercel connection

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
-    return (
-        <div className='flex justify-between  bg-drkGreen px-[70px] py-8 w-full gap-4 
+  return (
+    <div
+      className="flex justify-between  bg-drkGreen px-[70px] py-8 w-full gap-4 
         max-tablet:flex-col 
         max-tablet:justify-center 
-        max-tablet:items-center'>
-            <div>
-                <Image alt='Logo' src="/madasa_logo.svg" width={175} height={200}></Image>
-                <p className='text-white font-bold text-4xl'>Collective</p>
-            </div>
-            <div className='text-right text-white
-            max-tablet:text-left'>
-                <p>Madasa Collective</p>
-                <p>Made in CA</p>
-                <p>contact@madasacollective.com</p>
-            </div>
-        </div>
-    )
-}
+        max-tablet:items-center"
+    >
+      <div>
+        <Image
+          alt="Logo"
+          src="/madasa_logo.svg"
+          width={175}
+          height={200}
+        ></Image>
+        <p className="text-white font-bold text-4xl">Collective</p>
+      </div>
+      <div
+        className="text-right text-white
+            max-tablet:text-left"
+      >
+        <p>Madasa Collective HELLO</p>
+        <p>Made in CA</p>
+        <p>contact@madasacollective.com</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
