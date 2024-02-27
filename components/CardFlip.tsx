e67ce7637ebe6be6d329650767f9932d984a08d9
radodd <ethanf.flores@gmail.com>
2024-02-27 14:46:54 -0800

    animate card flip on hover

---
+++ components/CardFlip.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { cards } from "..";
+
+const CardFlip = () => {
+  return (
+    <div
+      className="container flex flex-row items-center gap-[111px] px-[70px] z-10
+    max-wide:flex-col
+    max-tablet:px-8
+    max-tablet:gap-8"
+    >
+      {cards.map((item) => (
+        <div
+          key={item.title}
+          className="card flex flex-col gap-6 shadow-br2xl px-16   
+       max-tablet:max-w-[320px]
+       max-tablet:min-w-[320px]
+       max-tablet:px-10"
+        >
+          <div className="front flex flex-col gap-6">
+            <Image src={item.icon} alt="card" width={256} height={256} />
+            <h1
+              className="text-4xl font-bold
+       max-tablet:text-2xl"
+            >
+              {item.title}
+            </h1>
+          </div>
+          <div className="back p-6">
+            <p className="text-xl text-left">{item.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardFlip;
