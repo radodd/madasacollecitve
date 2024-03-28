255fe4d78f87d2f38cd95fcc28dc95c4c7d31bde
radodd <ethanf.flores@gmail.com>
2024-03-27 20:36:35 -0700

    our story section new images

--- sections/our-team/OurStory.tsx
+++ sections/our-team/OurStory.tsx
@@ -7,9 +7,9 @@ import React, { useState } from "react";
 
 const OurStory = () => {
   const { isActive } = useNav();
+  const isDesktop = useMediaQuery("(min-width: 1001px)");
   const isTablet = useMediaQuery("(max-width: 1000px)");
-  const isDesktop = useMediaQuery("(min-width: 1000px)");
-  const isMobile = useMediaQuery("(max-width: 740px)");
+  const isMobile = useMediaQuery("(max-width: 739px)");
 
   const [collapse, setCollapse] = useState(false);
 
@@ -20,30 +20,90 @@ const OurStory = () => {
     <div
       className={`${
         isActive ? "blur" : ""
-      } flex justify-center items-center gap-14 px-70 py-[104px] bg-primary
+      } flex justify-center items-center gap-[36px] px-70 py-[104px] bg-primary
     max-[1000px]:gap-6
     max-[1000px]:px-8
     max-[1000px]:py-12 
-    max-extrasmall:flex-col`}
+    max-extrasmall:flex-col-reverse`}
     >
+      <div className="">
+        {isDesktop && !collapse && (
+          <Image
+            src="/ourstory_desktop.png"
+            width={441}
+            height={574}
+            alt="founder photo 1"
+            className="object-fill shadow-b2xl rounded-3xl min-w-[416px]"
+          />
+        )}
+        {isDesktop && collapse && (
+          <Image
+            src="/ourstory_desktop_open.png"
+            width={441}
+            height={724}
+            alt="founder photo 2"
+            className="object-fill shadow-b2xl rounded-3xl min-w-[416px]"
+          />
+        )}
+
+        {isTablet && !isMobile && collapse && (
+          <Image
+            src="/ourstory_tablet_open.png"
+            width={441}
+            height={636}
+            alt="founder photo 2"
+            className="shadow-b2xl rounded-3xl min-w-[373px]"
+          />
+        )}
+        {isTablet && !isMobile && !collapse && (
+          <Image
+            src="/ourstory_tablet.png"
+            width={441}
+            height={636}
+            alt="founder photo 2"
+            className="shadow-b2xl rounded-3xl min-w-[282px]"
+          />
+        )}
+      </div>
+      <div className="">
+        {isMobile && (
+          <Image
+            src="/ourstory.png"
+            width={329}
+            height={400}
+            alt="founder photo 3"
+            className="object-fill shadow-b2xl rounded-3xl"
+          />
+        )}
+      </div>
       <div
-        className="flex flex-col gap-8 py-14 px-[70px] bg-white rounded-3xl max-w-[900px] shadow-b2xl
-      max-[1000px]:px-8
-      max-[1000px]:py-8
+        className="flex flex-col justify-center p-16 bg-white rounded-3xl min-h-[474px] shadow-b2xl
+        w-full 
+        max-tablet:min-h-[339px]
+        max-[1000px]:px-8
+        max-[1000px]:py-8
+        
       "
       >
-        <h1 className="text-5xl font-bold max-[1000px]:text-4xl text-secondary">
+        <h1
+          className="text-5xl font-bold max-[1000px]:text-4xl text-secondary pb-8 tablet:pb-16
+        max-tablet:pb-12"
+        >
           The Story
         </h1>
-        <p className="text-2xl text-pretty max-[1000px]:text-xl">
+        <p className="text-4xl text-pretty tracking-[-0.01em]  max-[1000px]:text-xl">
           A Product Designer met a Software Developer in Santa Barbara, CA, and
           founded...
-          <span className="text-secondary font-bold">
+          <span className="text-secondary font-bold text-pretty">
             a digital powerhouse.{" "}
           </span>
         </p>
 
-        <div className={`${collapse ? "hidden" : ""} flex justify-center`}>
+        <div
+          className={`${collapse ? "hidden" : ""} flex justify-center pt-8
+        tablet:pt-16
+        max-tablet:pt-12`}
+        >
           {/* <Button
             type="button"
             title="THIS WILL BE A DOWN CHEVRON"
@@ -63,7 +123,7 @@ const OurStory = () => {
         <div
           className={`${collapse ? "expanded" : ""} flex flex-col long-text`}
         >
-          <p className="text-xl text-pretty">
+          <p className="text-xl text-pretty pt-6">
             Inspired by our previous backgrounds as a Structural Engineer
             (Serina) and Army Combat Engineer Veteran (Ethan),{" "}
             <span className="font-semibold">
@@ -99,38 +159,6 @@ const OurStory = () => {
       </div>
 
       {/* See More Content */}
-
-      <div className="">
-        {isDesktop && (
-          <Image
-            src="/ourstory.png"
-            width={416}
-            height={544}
-            alt="founder photo 1"
-            className="object-fill shadow-b2xl rounded-3xl min-w-[416px]"
-          />
-        )}
-        {isTablet && !isMobile && (
-          <Image
-            src="/ourstory_tablet.png"
-            width={373}
-            height={636}
-            alt="founder photo 2"
-            className="shadow-b2xl rounded-3xl min-w-[373px]"
-          />
-        )}
-      </div>
-      <div className="max-w-[329px]">
-        {isMobile && (
-          <Image
-            src="/ourstory.png"
-            width={329}
-            height={400}
-            alt="founder photo 3"
-            className="object-fill shadow-b2xl rounded-3xl"
-          />
-        )}
-      </div>
     </div>
   );
 };
